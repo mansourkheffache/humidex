a326c08d7bfc77e5a0bfd3abbf7f0d252a0c5323
Mansour Kheffache <mansourkheffache@gmail.com>
2017-12-11 15:50:27 +0100

    created tables and websocket output interface working

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
-from flask import Flask
-import socket
+#!/usr/bin/python
+import tornado.ioloop
+import tornado.websocket
 
-app = Flask(__name__)
+import sqlite3
+import json
+ 
+conn = sqlite3.connect('local.db')
+conn.row_factory = sqlite3.Row
+c = conn.cursor()
 
-@app.route('/')
-def hello():
-	return 'Hello World!'
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+ 
+    def open(self):
+        print("WebSocket opened")
+ 
+    def on_message(self, message):
 
+        req = json.loads(message)
 
-if __name__ == '__main__':
+        if req['type'] == 'query':
+            t = (req['time'],)
+            c.execute('SELECT * FROM entries WHERE timestamp>? ORDER BY timestamp DESC', t)
+            results = c.fetchmany(size=20)
 
+            res = {'message': [dict(results[i]) for i in range(0, len(results))]}
+            self.write_message(res)
 
-	app.run()
+    def on_close(self):
+        print("WebSocket closed")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/websocket", EchoWebSocket),
+    ])
+
+def data_in():
+	# read arduino
+
+	# read SNMP
+
+	# write to db
+	pass
+
+def data_out():
+	pass
+ 
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(9999)
+    tornado.ioloop.IOLoop.current().start()
