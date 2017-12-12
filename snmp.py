a03de0bbe6ca081a9950a7661b20a0abf9d75f7a
florianakos <florian.akos.szabo@gmail.com>
2017-12-12 12:06:07 +0100

    Optimizing SNMP

--- snmp.py
+++ snmp.py
@@ -7,15 +7,11 @@ from datetime import datetime
 # RETURNS A SUM OF ACTUAL POWER FOR ALL OUTLETS IN WATTS
 def getPower():
     session = Session(hostname="10.10.10.151", community='perccom', version=2)
-    energy_sum = 0
+    sum = 0
     for i in range(1,9):
-        system_items = session.walk('1.3.6.1.4.1.13742.6.5.4.3.1.4.1.' + str(i))
-        counter = 1
-        for item in system_items:
-            if counter == 3:
-                energy_sum += int(item.value)
-            counter += 1
-    return energy_sum
+        snmp_response = session.get('1.3.6.1.4.1.13742.6.5.4.3.1.4.1.' + str(i) + ".5")
+        sum += int(snmp_response.value)
+    return sum
 
 
 # QUERY THE SWITCH (10.10.10.1) FOR IN AND OUT BYTES
@@ -23,21 +19,23 @@ def getPower():
 def getBytes(ip):
     session = Session(hostname=ip, community='perccom', version=2)
     # IN OCTETS
-    system_items = session.walk('1.3.6.1.2.1.2.2.1.10')
     inOctets = 0
-    i = 0
-    for item in system_items:
-        if i is not  0:
-            inOctets += int(item.value)
-        i+=1
+    for i in range(1,25):
+        if i < 10:
+            snmp_response = session.get('1.3.6.1.2.1.2.2.1.10.1010' + str(i))
+        else:
+            snmp_response = session.get('1.3.6.1.2.1.2.2.1.10.101' + str(i))
+        inOctets += int(snmp_response.value)
+
     # OUT OCTETS
-    system_items = session.walk('1.3.6.1.2.1.2.2.1.16')
     outOctets = 0
-    j = 0
-    for item in system_items:
-        if j is not  0:
-            outOctets += int(item.value)
-        j+=1
+    for i in range(1,25):
+        if i < 10:
+            snmp_response = session.get('1.3.6.1.2.1.2.2.1.16.1010' + str(i))
+        else:
+            snmp_response = session.get('1.3.6.1.2.1.2.2.1.16.101' + str(i))
+        outOctets += int(snmp_response.value)
+
     # RETURN THE SUM
     return inOctets + outOctets
 
